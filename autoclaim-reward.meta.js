9e6d79f4d6233abf0e13f706232faec4d71c672d
crashmax-dev <crashmax.dev@gmail.com>
2022-11-20 09:03:05 +0000

    Deploying to gh-pages from @ crashmax-dev/autoclaim-reward@d7426c552f7cf75ae54fbd2578f94587d2be0a5f 🚀

---
+++ autoclaim-reward.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        autoclaim-reward
+// @version     0.0.1
+// @license     MIT
+// @homepage    https://crashmax-dev.github.io/autoclaim-reward/
+// @match       https://*.twitch.tv/*
+// @updateURL   https://crashmax-dev.github.io/autoclaim-reward/autoclaim-reward.meta.js
+// @downloadURL https://crashmax-dev.github.io/autoclaim-reward/autoclaim-reward.user.js
+// ==/UserScript==
